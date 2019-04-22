d123ab86a3fb31a4f38508770f866bd0bfd1097b
Kish Siva <rodes88@msn.com>
2019-04-22 19:28:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
   
-  def name 
-    @name 
+  def name
+    @name
   end
-  
-  def name =(name)
-    @name = name
-  end 
+ 
+  def name=(new_name)
+    @name = new_name
+  end
   
 end
